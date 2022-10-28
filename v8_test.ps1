6ab430c17654abb7be54bbe35319089876706f31
Xingwang Liao <kuoruan@gmail.com>
2022-10-28 23:19:29 +0800

    feat: update build args

--- v8_test.ps1
+++ v8_test.ps1
@@ -6,7 +6,7 @@ if ( -not ( Test-Path -Path "$PSScriptRoot\v8" ) ) {
 
 g++ -I"$PSScriptRoot`\v8" -I"$PSScriptRoot`\v8\include" `
     "$PSScriptRoot`\v8\samples\hello-world.cc" -o hello_world.exe `
-    -lv8_monolith -L"$PSScriptRoot" -L"$PSScriptRoot`\v8\out\release\obj" `
+    -lv8_monolith -L"$PSScriptRoot`\v8\out\release\obj\" `
     -pthread -std=c++17 -ldl -DV8_COMPRESS_POINTERS
 
 Invoke-Expression -Command ".\hello_world.exe"
--- v8_test.sh
+++ v8_test.sh
@@ -13,7 +13,7 @@ fi
 	set -x
 	g++ -I"${dir}/v8" -I"${dir}/v8/include" \
 		"${dir}/v8/samples/hello-world.cc" -o hello_world \
-		-lv8_monolith -L"$dir" -L"${dir}/v8/out/release/obj" \
+		-lv8_monolith -L"${dir}/v8/out/release/obj/" \
 		-pthread -std=c++17 -ldl -DV8_COMPRESS_POINTERS
 )
 
