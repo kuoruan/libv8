21f4ef7b39f56812bddb3d7bb5c38a950edcd3f2
Xingwang Liao <kuoruan@gmail.com>
2024-11-13 06:46:36 +0000

    test(v8): use Common Language Runtime Compilation for windows

--- v8_test.bat
+++ v8_test.bat
@@ -9,7 +9,7 @@ if not exist "%dir%\v8" (
   exit /b 1
 )
 
-call cl.exe /EHsc /std:c++20 /I"%dir%\v8" /I"%dir%\v8\include" ^
+call cl.exe /EHsc /clr /std:c++20 /I"%dir%\v8" /I"%dir%\v8\include" ^
   /Fe".\hello-world" "%dir%\v8\samples\hello-world.cc" ^
   /link "%dir%\v8\out\release\obj\v8_monolith.lib" ^
   /DEFAULTLIB:advapi32.lib /DEFAULTLIB:dbghelp.lib /DEFAULTLIB:winmm.lib
